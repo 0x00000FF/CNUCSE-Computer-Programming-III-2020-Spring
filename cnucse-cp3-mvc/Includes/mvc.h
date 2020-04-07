c69a4a488d2fb3b0a0af244f7eb21a39fddce52f
Kangjun Heo == P.Knowledge <knowledge@o.cnu.ac.kr>
2020-04-08 02:08:04 +0900

    컴프3MVC: 기본 OOP 문법 및 Flow 정의

---
+++ cnucse-cp3-mvc/Includes/mvc.h
@@ -0,0 +1,11 @@
+#ifndef __MVC_H__
+#define __MVC_H__
+
+#include <assert.h>
+#include <stdlib.h>
+
+#include <mvc/oop.h>
+
+int mvc_main(int argc, char* argv[]);
+
+#endif
---
+++ cnucse-cp3-mvc/Sources/main.c
@@ -0,0 +1,5 @@
+#include <mvc.h>
+
+int main(int argc, char* argv[]) {
+	return mvc_main(argc, argv);
+}
