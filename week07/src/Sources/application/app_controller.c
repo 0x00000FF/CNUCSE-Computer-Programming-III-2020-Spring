ff9534dc9c0a19d14070b06115fd1c6f21482a8d
Kangjun Heo == P.Knowledge <knowledge@o.cnu.ac.kr>
2020-04-29 22:17:10 +0900

    7주차 실습: 프로젝트 생성

---
+++ week07/src/Sources/application/app_controller.c
@@ -0,0 +1,27 @@
+﻿#include <mvc/app_controller.h>    // app_controller
+
+// app_controller 생성자
+app_controller* app_controller_create(int argc, char* argv)
+{
+	app_controller* controller = (app_controller*)malloc(sizeof(app_controller));
+
+	controller->argc   = argc;
+	controller->argv   = argv;
+	controller->result = 0;
+
+	return controller;
+}
+
+// application routine 시작
+void            app_controller_run   (app_controller* self)
+{
+
+}
+
+// app_controller 소멸자
+void            app_controller_delete(app_controller* self)
+{
+	if (self == NULL) return;
+
+	free(self);
+}
---
+++ week07/src/Sources/main.c
@@ -0,0 +1,20 @@
+﻿#include <mvc/app_controller.h>           // app_controller
+
+// 진입점
+int main(int argc, char* argv[])
+{
+	// state, app_controller 객체 생성
+	int             result     = 0;
+	app_controller* controller = app_controller_create(argc, argv);
+	
+	// app_controller_exit() 구현을 위한 setjmp
+	if (setjmp( *app_jmp_env() ) == 0) {
+		// 실행, 결과 수신, 객체 해제
+		app_controller_run(controller);
+		
+		result = app_controller_get_result(controller);
+		app_controller_delete(controller);
+	}
+
+	return result;
+}
