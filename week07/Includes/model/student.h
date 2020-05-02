78044e86bee7abdeb07472f91a12f5cce066a977
Kangjun Heo == P.Knowledge <knowledge@o.cnu.ac.kr>
2020-05-02 17:56:46 +0900

    7주차 실습: student_id 타입 uint64_t -> char[10]

--- week07/Includes/model/student.h
+++ week07/Includes/model/student.h
@@ -1,12 +1,13 @@
 #pragma once
 
-#include <stdint.h>
+#include <stdbool.h>
+#define  STUDENT_ID_LENGTH 9
 
-typedef uint64_t student_id;
+typedef char student_id[STUDENT_ID_LENGTH + 1];
 
 typedef struct {
-	student_id id;
-	int        score;
+	student_id      id;
+	int             score;
 } student;
 
 student*       student_create            (student_id id, int score);
@@ -14,3 +15,5 @@ void           student_delete            (student*   self);
 const char*    student_get_student_id    (student*   self);
 int            student_get_score         (student*   self);
 
+bool           student_is_id_valid       (student_id id);
+bool           student_is_score_valid    (int score);
--- week07/Sources/application/model/student.c
+++ week07/Sources/application/model/student.c
@@ -11,7 +11,7 @@ student* student_create(student_id id, int score)
 		return NULL;
 	}
 
-	new_student->id    = id;
+	strcpy_s(new_student->id, sizeof(id), id);
 	new_student->score = score;
 
 	return new_student;
@@ -32,4 +32,14 @@ const char* student_get_student_id(student* self)
 int student_get_score(student* self)
 {
 	return self->score;
+}
+
+bool student_is_id_valid(student_id id)
+{
+	return strlen(id) >= STUDENT_ID_LENGTH;
+}
+
+bool student_is_score_valid(int score)
+{
+	return score >= 0 || score <= 100;
 }
