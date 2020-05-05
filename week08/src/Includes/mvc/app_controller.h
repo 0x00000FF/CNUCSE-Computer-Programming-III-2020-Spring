01508797876b0282590ad95117b076c74895939f
Kangjun Heo == P.Knowledge <knowledge@o.cnu.ac.kr>
2020-05-05 18:17:11 +0900

    8주차 실습: 7주차 클론

---
+++ week08/src/Includes/mvc/app_controller.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#include <stdlib.h>
+#include <setjmp.h>
+
+#include <model/lecture_class.h>
+
+#define  MAX_STUDENTS   100
+
+typedef struct {
+	int              argc, result;
+	char**           argv;
+
+	lecture_class*   students;
+} app_controller;
+
+
+jmp_buf*           app_jmp_env();
+
+app_controller*    app_controller_create      (int argc, char** argv);
+void               app_controller_run         (app_controller* self);
+void               app_controller_exit        (app_controller* self, int status);
+int                app_controller_get_result  (app_controller* self);
+void               app_controller_delete      (app_controller* self);
