e3473c5d58ad4e22af80cc4f170264852cc90d29
Kangjun Heo == P.Knowledge <knowledge@o.cnu.ac.kr>
2020-05-08 23:51:51 +0900

    8주차 실습: 불필요 파일 제거

--- week08-1/src/Includes/model/lecture_class.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <mvc/vector.h>
-#include <model/student.h>
-#include <app/vector_student.h>
-
-typedef VECTOR(student) lecture_class;
-
-void  lecture_class_sort_score(lecture_class* self);
