5897619d50cdfd9ce3d422da1269385dd90b74f7
Kangjun Heo == P.Knowledge <knowledge@o.cnu.ac.kr>
2020-05-14 21:55:38 +0900

    10주차 실습: 벡터가 두개 필요하네

--- week10/src/Includes/app/vector_char.h
+++ week10/src/Includes/app/vector_char.h
@@ -3,22 +3,20 @@
 #include <stdlib.h>
 #include <mvc/vector.h>
 
-typedef char element;
+VECTOR_DEF(char);
 
-VECTOR_DEF(element);
-
-FUNC_VECTOR_NEW(element);
-FUNC_VECTOR_DELETE(element);
-FUNC_VECTOR_AT(element);
-FUNC_VECTOR_FRONT(element);
-FUNC_VECTOR_BACK(element);
-FUNC_VECTOR_DATA(element);
-FUNC_VECTOR_EMPTY(element);
-FUNC_VECTOR_SIZE(element);
-FUNC_VECTOR_MAX_SIZE(element);
-FUNC_VECTOR_CLEAR(element);
-FUNC_VECTOR_INSERT(element);
-FUNC_VECTOR_ERASE(element);
-FUNC_VECTOR_PUSH_BACK(element);
-FUNC_VECTOR_POP_BACK(element);
-FUNC_VECTOR_SWAP(element);
+FUNC_VECTOR_NEW(char);
+FUNC_VECTOR_DELETE(char);
+FUNC_VECTOR_AT(char);
+FUNC_VECTOR_FRONT(char);
+FUNC_VECTOR_BACK(char);
+FUNC_VECTOR_DATA(char);
+FUNC_VECTOR_EMPTY(char);
+FUNC_VECTOR_SIZE(char);
+FUNC_VECTOR_MAX_SIZE(char);
+FUNC_VECTOR_CLEAR(char);
+FUNC_VECTOR_INSERT(char);
+FUNC_VECTOR_ERASE(char);
+FUNC_VECTOR_PUSH_BACK(char);
+FUNC_VECTOR_POP_BACK(char);
+FUNC_VECTOR_SWAP(char);
